8ae8747ab68f2ab1441c9013416beb13f28d57a5
kamilwin21 <kamilwin21@gmail.com>
2025-06-30 20:49:08 +0200

    modified index.js and adding db.js

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require('express');
+const pool = require('./db');
 const app = express();
 const port = process.env.PORT  || 3000;
+
+
+app.use(express.json());
+
+app.get('/users', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM users');
+        res.json(result.rows);
+    } catch (err) {
+        res.status(500).json({error: err.message});
+    }
+});
 
 app.get('/', (req, res) => {
     res.json({message: "Działa Render.com!"});
