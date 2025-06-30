c0b157e862d7aad311937655f50bfe3f9fcf7421
kamilwin21 <kamilwin21@gmail.com>
2025-06-30 20:56:50 +0200

    new post endpoint adding new user

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/users', async (req, res) => {
     }
 });
 
-app.post('users', async (req, res) => {
+app.post('/users', async (req, res) => {
     const { name, email } = req.body;
     try {
         const result = await pool.query(
