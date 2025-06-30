30d9b0dde18fab13f9fcb9daee352a4c435ed951
kamilwin21 <kamilwin21@gmail.com>
2025-06-30 20:54:53 +0200

    new post endpoint adding new user

--- index.js
+++ index.js
@@ -15,6 +15,19 @@ app.get('/users', async (req, res) => {
     }
 });
 
+app.post('users', async (req, res) => {
+    const { name, email } = req.body;
+    try {
+        const result = await pool.query(
+            'INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *',
+            [name, email]
+        );
+        res.status(201).json(result.rows[0]);
+    } catch (err) {
+        res.status(500).json({error: err.message});
+    }
+});
+
 app.get('/', (req, res) => {
     res.json({message: "Działa Render.com!"});
 });
